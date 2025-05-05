2da8bf930abb4922c83434e113d2bca310481c69
Vickynina <vickynina006@gmail.com>
2025-05-06 05:06:21 +1200

    second commit

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -2,11 +2,12 @@ export default function Button({
   href,
   title,
   bg = "bg-gradient-to-r from-bgGreen2 from-[30%] to-bgGreen3 to[70%] ",
+  styles = "px-7 py-2",
 }) {
   return (
     <a href={href}>
       <button
-        className={`text-lg rounded-sm outline outline-1 outline-slate-400 text-white px-7 py-2 ${bg} hover:text-[#c5eee4] hover:outline-[#c5eee4]`}
+        className={`text-lg rounded-sm outline outline-1 outline-slate-400 text-white ${styles} ${bg} hover:text-[#c5eee4] hover:outline-[#c5eee4]`}
       >
         {title}
       </button>
