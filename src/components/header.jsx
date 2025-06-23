bb9cf61b6371d62bf40d80efd203b0dfb94ba168
Vickynina <vickynina006@gmail.com>
2025-06-24 07:38:23 +1200

    fixed nav blur

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,19 +1,36 @@
 import { NavLink } from "react-router-dom";
 import logo from "../assets/logo.png";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NavModal from "./navModal";
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
   function handleClick() {
     setIsOpen((prev) => !prev);
   }
+  useEffect(() => {
+    function handleScroll() {
+      // if(window.scrollY > 10){
+      //   setScrolled(true)
+      // }
+      setScrolled(window.scrollY > 10);
+    }
 
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   const spanStyle =
     "w-6 h-[3px] bg-slate-200 rounded-sm transition-all duration-300 ease-in-out";
 
   return (
-    <header className="flex items-center  justify-between fixed top-0 left-0 right-0 px-8 py-10  md:px-16    lgx:px-24">
+    <header
+      className={`flex items-center  justify-between fixed top-0 left-0 right-0 px-8 md:px-16 lgx:px-24 ${
+        scrolled ? "bg-bgGreen2/10 py-5 backdrop-blur-md" : "py-10"
+      } `}
+    >
       <img src={logo} alt="Logo" className=" w-32 h-10 md:w-[10rem] md:h-14" />
       <nav>
         <ul className="hidden items-center gap-5 text-[1.05rem] text-slate-100 lg:gap-7 mdx:flex">
